1cec2579be52179b4756cb1d42e3349331892ee5
Antonino Ingargiola <antonino.ingargiola@agilelab.it>
2025-03-19 14:53:26 +0100

    add autoformat

--- validate_examples.py
+++ validate_examples.py
@@ -1,19 +1,22 @@
+import sys
+
 import requests
 import yaml
-import sys
 from openapi_core import OpenAPI
 from openapi_core.contrib.requests import RequestsOpenAPIRequest
 
+
 def pad_string(s: str, sep: str, ln: int = 50) -> str:
-  """
-  Pads the input string `s` with the specified character `sep` until the total length is 50 characters.
-  Args:
-      s (str): The input string to be padded.
-      sep (chr): The character used for padding.
-  Returns:
-      str: The padded string with a total length of 50 characters.
-  """
-  return f"{s}{sep * (ln - len(s))}"
+    """
+    Pads the input string `s` with the specified character `sep` until the total length is 50 characters.
+    Args:
+        s (str): The input string to be padded.
+        sep (chr): The character used for padding.
+    Returns:
+        str: The padded string with a total length of 50 characters.
+    """
+    return f"{s}{sep * (ln - len(s))}"
+
 
 files = ['examples/health-care.yaml']
 openapi = OpenAPI.from_file_path('agent-specification.yaml')
@@ -25,7 +28,7 @@ for fn in files:
     request = requests.Request(
         method='POST',  # Change to your HTTP method
         url='http://www.agents.com/agents',  # Must match spec
-        json=body_content
+        json=body_content,
     )
     openapi_request = RequestsOpenAPIRequest(request)
     try:
@@ -34,7 +37,7 @@ for fn in files:
     except Exception as e:
         anyError = True
         if e.__cause__:
-          e = e.__cause__
+            e = e.__cause__
         print(pad_string(fn, '.') + f" ❌ Not validated\n{e}", file=sys.stderr)
 
 exit(anyError)
